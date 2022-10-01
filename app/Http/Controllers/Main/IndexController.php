e808d81ca0689e6559243b5072383620aebfaec0
Madi <tabynbay.madi@mail.ru>
2022-10-01 16:21:35 +0600

    added newsportal

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -9,6 +9,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return 'bbbb';
+        return 'cccc';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
+
+Auth::routes();
