f1efdf87f3714bca6c559c96ae3304115953e77b
Madi <tabynbay.madi@mail.ru>
2022-11-17 06:00:19 +0600

    added postsliked

--- app/Models/PostUserLikes.php
+++ app/Models/PostUserLikes.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostUserLike extends Model
+class PostUserLikes extends Model
 {
     use HasFactory;
-    protected $table = 'post_tags';
+    protected $table = 'post_user_likes';
     protected $guarded = false;
 }
